fde8e6a01d090a5810e937b35f8be15ac015fdef
Renasal07 <1003011@pdsb.net>
2025-02-27 13:34:50 -0500

    readme change

--- 05_DayoftheWeekActivityRecommender/DayoftheWeekActivityRecommender.py
+++ 05_DayoftheWeekActivityRecommender/DayoftheWeekActivityRecommender.py
@@ -9,17 +9,24 @@
 #-----------------------------------------------------------------------------
 
 day = input("Enter the current day of the week (e.g., Monday, Tuesday): ")
+# If the day is "Monday", print: `"Start your week with a workout!"
 if day == "Monday":
     print("Start your week with a workout!")
+# If the day is "Tuesday", print: `"It's a great day to read a book!"`
 if day == "Tuesday":
     print("It's a great day to read a book!")
+# If the day is "Wednesday", print: `"Mid-week movie night!"`
 if day == "Wednesday":
     print("Mid-week movie night!")
+# If the day is "Thursday", print: `"Try a new recipe!"`
 if day == "Thursday":
     print("Try a new recipe!")
+# If the day is "Friday", print: `"Relax and enjoy the weekend!"`
 if day == "Friday":
     print("Relax and enjoy the weekend!")
+#  If the day is "Saturday", print: `"Go for a hike!"`
 if day == "Saturday":
     print("Go for a hike!")
+#  If the day is "Sunday", print: `"Prepare for the week ahead with some self-care."`
 if day == "Sunday":
     print("Prepare for the week ahead with some self-care.")
